d82a3863e47ece075ab847778e7b0997cc66464d
Tim <timgomperts@gmail.com>
2024-09-27 15:24:34 +0200

    Write test for DNI letter calculation in inscripcio component

---
+++ src/app/components/inscripcio/inscripcio.component.spec.ts
@@ -0,0 +1,31 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { InscripcioComponent } from './inscripcio.component';
+
+describe('InscripcioComponent', () => {
+  let component: InscripcioComponent;
+  let fixture: ComponentFixture<InscripcioComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [InscripcioComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(InscripcioComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should calculate DNI letter correctly', () => {
+    const component = new InscripcioComponent();
+    component.inscripcioForm.controls['dni'].setValue('12345678');
+    component.calculateDNILetter();
+    expect(component.dniLetter).toBe('Z');
+  });
+  
+});
---
+++ src/app/components/inscripcio/inscripcio.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inscripcio',
+  standalone: true,
+  imports: [],
+  templateUrl: './inscripcio.component.html',
+  styleUrl: './inscripcio.component.css'
+})
+export class InscripcioComponent {
+
+}
